93e2e09bc0057d0031acb5062c4be4c48827cdd1
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-16 14:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   recipes['prop'] = 1;
   recipes['prop2'] = 2
-  return recipes
 }
