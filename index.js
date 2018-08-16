bf72ab0acbf5f6dfe6707d8e5f137852ac9de800
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-16 14:10:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {typeof: 'object'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({prop: 1}, {prop2: 2})
-  recipes.prop2 = '2';
+  
 }
 
 function 
